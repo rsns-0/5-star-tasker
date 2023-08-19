ba4f573c02ad450ae5f6bd2d6b0fe6f7a96135d9
sn <126475703+rsns-0@users.noreply.github.com>
2023-08-19 01:00:31 -0600

    Move events into separate folder

---
+++ src/backend/features/discordJs/events/interactionCreate.ts
@@ -0,0 +1,33 @@
+import {CommandInteraction, Events} from "discord.js"
+
+import ReadyClient from "../models/client";
+
+const event = {
+    name: Events.InteractionCreate,
+    async execute(interaction:CommandInteraction) {
+        if (!interaction.isChatInputCommand()) return;
+        const client = interaction.client as ReadyClient
+        const command = client.commands.get(interaction.commandName);
+    
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: "There was an error while executing this command!",
+                    ephemeral: true,
+                });
+            } else {
+                await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+            }
+        }
+    }
+}
+
+export default event
