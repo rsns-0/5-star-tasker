76c4bbe0eed1abca8660cd5d0d2ce004bcbc5321
sn <126475703+rsns-0@users.noreply.github.com>
2023-08-25 02:43:00 -0600

    Refactored code for better readability and error handling
    - Not yet tested

--- src/backend/features/discordJs/events/translationReactions.ts
+++ src/backend/features/discordJs/events/translationReactions.ts
@@ -1,10 +1,64 @@
-import { Events } from 'discord.js';
+import {
+	Events,
+	MessageReaction,
+} from "discord.js";
+
+import { LanguageRepository } from "../../translation/models/languageRepository";
+import { Logger } from "@/backend/logger/logger";
+import { TranslationService } from "../../translation/services/translationService";
+import { TranslationServiceError } from "../../translation/models/translationServiceError";
+import { createTranslationEmbed } from "../utils/createTranslationEmbed";
 
 const name = Events.MessageReactionAdd;
-const execute = async (message:any, user:any) => {
+const translation = new TranslationService();
+const logger = new Logger();
+const DEFAULT_ACCENT_COLOR = 0x0099ff;
+
+
+/**
+ * 
+ * This event handler translates a target message when a user reacts to it with an emoji and replies to the reacter with the translation.
+ * 
+ */
+const execute = async (reaction: MessageReaction) => {
+	const { channel } = reaction.message;
+
+	const textToTranslate = reaction.message.content;
+	const emojiReactionID = reaction.emoji.name;
+
+	if (textToTranslate === null) {
+		throw new Error("Unexpected null value in text to translate.");
+	}
+	if (emojiReactionID === null) {
+		throw new Error("Unexpected null value in emoji reaction ID.");
+	}
 	
-	console.log(message, user);
+	const targetLanguage =
+		LanguageRepository.getInstance().getLanguageIfAvailableForTranslation(emojiReactionID);
+	if (!targetLanguage) {
+		return; // no throwing since user can react with any emoji
+	}
+
+	const result = await translation.translateTextWithValidation({
+		text: textToTranslate,
+		targetLanguage,
+	});
+	if (result instanceof TranslationServiceError) {
+		const { message } = result.autoResolve();
+		await channel.send(message)
+		return;
+	}
+
+	const { sourceLanguage, text } = result[0];
+	const color = reaction.client.user.accentColor || DEFAULT_ACCENT_COLOR;
+	const embed = createTranslationEmbed(text, sourceLanguage, targetLanguage, color);
+
+	try {
+		await reaction.message.reply({ embeds: [embed] })
+	} catch (e) {
+		logger.logError(e);
+		await channel.send("An unknown server error occurred. Please try again later.");
+	}
 };
 
-
-export { name, execute }	
+export { name, execute };
