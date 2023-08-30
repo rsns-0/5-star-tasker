4ba0be4584c6766f45fc6a531c8560c1a9aa5f4f
Matuz#0117/ <109036200+Rodrigo-Matuz@users.noreply.github.com>
2023-08-29 21:49:18 -0300

    reminder command working 99% with dayjs library

--- src/backend/features/discordJs/commands/reminder.ts
+++ src/backend/features/discordJs/commands/reminder.ts
@@ -1,5 +1,14 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-import timeStringToDateObj from "../services/stringToDateObj";
+import {
+    ActionRowBuilder,
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+    StringSelectMenuBuilder,
+} from "discord.js";
+import timeStringToDayjsObj from "../services/stringToDayjsObj";
+import {
+    timezonesNegatives,
+    timezonesPositives,
+} from "../models/selectBoxForTimezones";
 
 export const data = new SlashCommandBuilder()
     .setName("reminder")
@@ -9,6 +18,7 @@ export const data = new SlashCommandBuilder()
             .setName("time")
             .setDescription("send 0 (zero) for better explanation")
             .setRequired(true)
+            .setMaxLength(50)
     )
     .addStringOption(
         (option) =>
@@ -23,17 +33,50 @@ export const data = new SlashCommandBuilder()
 
 export const execute = async (interaction: ChatInputCommandInteraction) => {
     const timeString = interaction.options.getString("time")!;
+    const row1 = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+        timezonesNegatives
+    );
+    const row2 = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+        timezonesPositives
+    );
     if (timeString === "0") {
         interaction.reply({
             content: "Explanation",
             ephemeral: true,
         });
     } else {
-        const timezone = "America/New_York";
-        const dateObject = timeStringToDateObj(timeString, timezone);
-        interaction.reply({
-            content: dateObject.toString(),
+        const response = await interaction.reply({
+            content: "Select your timezone",
+            components: [row1.toJSON(), row2.toJSON()],
             ephemeral: true,
         });
+
+        const collectorFilter = (i: any) => i.user.id === interaction.user.id;
+        try {
+            const confirmation: any = await response.awaitMessageComponent({
+                filter: collectorFilter,
+                time: 60000,
+            });
+            if (
+                confirmation.customId == "positives" ||
+                confirmation.customId == "negatives"
+            ) {
+                const dayJsObj = timeStringToDayjsObj(
+                    timeString.toLowerCase(),
+                    confirmation.values
+                );
+                await confirmation.update({
+                    content: `Reminder Added Successfully <t:${dayJsObj.unix()}:F>`,
+                    components: [],
+                });
+            }
+        } catch (e) {
+            await interaction.editReply({
+                content:
+                    "You had 1 minute to select your timezone, please try again later",
+                components: [],
+            });
+            console.log(e);
+        }
     }
 };
