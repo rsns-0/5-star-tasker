6b63ac5ff240d46e2143deb31c307f2f1a748f26
sn <126475703+rsns-0@users.noreply.github.com>
2023-08-30 22:11:50 -0600

    convenience fn for logging

---
+++ src/utils/logTime.ts
@@ -0,0 +1,15 @@
+import { formatInTimeZone } from "date-fns-tz";
+
+type DateArgs = Parameters<typeof formatInTimeZone>[0]
+
+export function logTime(time:DateArgs){
+    
+    console.log(createLogTimeMessage(time))
+}
+
+export function createLogTimeMessage(time:DateArgs){
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    const res = formatInTimeZone(time, timezone, "yyyy-MM-dd hh:mm:ss a")
+    const msg = `${res} (Estimated timezone: ${timezone})`
+    return msg
+}
