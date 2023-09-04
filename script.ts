6f2ef1edaf9a7ea494ac96edf872d9f3059cb991
sn <126475703+rsns-0@users.noreply.github.com>
2023-09-04 07:21:46 -0600

    c

---
+++ script.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.post('https://discord.com/api/webhooks/1148160923220054086/ZetxH_PL7e8s4trONE0xQnT16EhCZHnIB3Px9oGc_r7jLWBz67wkmbM60lpsnUVnT6yt', {
+	content: 'hello worldasdad'
+});
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,9 @@
+import { CategoryChannel, GuildBasedChannel, PrivateThreadChannel, PublicThreadChannel } from 'discord.js';
+
+import { ThreadChannel } from './types';
+
 export type ValueOf<T> = T[keyof T];
 
 export type PublicKeyOf<T> = Exclude<keyof T, `_${string}`>;
+export type ThreadChannel = PrivateThreadChannel | PublicThreadChannel;
+export type WebhookChannel = Exclude<GuildBasedChannel, ThreadChannel | CategoryChannel>;
