82047a9104d3fa7fadf791a87184be4a4b5069b7
sn <126475703+rsns-0@users.noreply.github.com>
2023-09-05 19:04:52 -0600

    c

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,8 +2,14 @@ import { createLogger, format, transports } from 'winston';
 
 import prisma from '../db/prismaInstance';
 
+const dirname = 'testLogs';
+
+const errorFileTransport = new transports.File({ filename: 'error.log', dirname, level: 'error' });
+const combinedFileTransport = new transports.File({ filename: 'combined.log', dirname, level: 'debug' });
+const infoFileTransport = new transports.File({ filename: 'info.log', dirname, level: 'info' });
+const consoleTransport = new transports.Console({});
 const logger = createLogger({
-	transports: [new transports.Console(), new transports.File({ filename: 'combined.log' })],
+	transports: [consoleTransport, errorFileTransport, combinedFileTransport, infoFileTransport],
 	format: format.combine(format.timestamp(), format.json(), format.prettyPrint()),
 	level: 'debug'
 });
