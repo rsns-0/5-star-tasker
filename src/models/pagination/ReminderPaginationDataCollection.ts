06bce0fd73145774ae5eaa5fa754c7219831b2bd
sn <126475703+rsns-0@users.noreply.github.com>
2023-09-13 15:52:23 -0600

    c

--- src/models/pagination/ReminderPaginationDataCollection.ts
+++ src/models/pagination/ReminderPaginationDataCollection.ts
@@ -1,14 +1,8 @@
-import {
-	PaginatedMessageAction,
-	PaginatedMessageActionContext,
-} from "@sapphire/discord.js-utilities";
-import { ButtonStyle, Collection, ComponentType, ModalBuilder } from "discord.js";
-import { ReminderComponentData, ReminderPage } from "models/pagination/reminderComponentData";
+import { Collection } from "discord.js";
+import { ReminderPage } from "models/pagination/reminderComponentData";
 
 import { reminders } from "@prisma/client";
 import { paginateArrays } from "../../utils/paginateArrays";
-import { CustomId } from "./CustomId";
-import { createReminderRowComponent } from "./paginationRowData";
 
 /**
  * This class paginates the reminder data and maps their index to data required for building the embeds and their page actions.
@@ -29,59 +23,8 @@ export class ReminderPages extends Collection<number, ReminderPage> {
 		const remindersPaginated = paginateArrays(reminders, pageSize);
 		const reminderData = new this();
 		remindersPaginated.forEach((reminders, pageIndex) => {
-			reminderData.set(pageIndex, createReminderEmbedDataCollection(reminders));
+			reminderData.set(pageIndex, ReminderPage.fromReminders(reminders));
 		});
 		return reminderData;
 	}
-}
-
-/**
- * Creates a collection of ReminderPaginationData objects from an array of reminders.
- * @param reminders An array of reminders.
- * @returns A Collection of ReminderPaginationData objects.
- */
-function createReminderEmbedDataCollection(reminders: reminders[]) {
-	const collection = new ReminderPage();
-	reminders.forEach((reminder, entryIndex) => {
-		collection.set(reminder.id, convertReminder(reminder, entryIndex));
-	});
-	return collection;
-}
-
-/**
- * Converts a reminder object to a ReminderPaginationData object.
- * @param reminder - The reminder object to convert.
- * @param entryIndex - The index of the entry.
- * @returns The converted ReminderPaginationData object.
- */
-function convertReminder(reminder: reminders, entryIndex: number): ReminderComponentData {
-	const data: ReminderComponentData = new ReminderComponentData({
-		id: reminder.id,
-		message: reminder.reminder_message,
-		time: reminder.time,
-		button: createButton(entryIndex, reminder),
-	});
-	return data;
-}
-
-function createButton(entryIndex: number, reminder: reminders): PaginatedMessageAction {
-	const row = createReminderRowComponent(reminder.reminder_message, "");
-	return {
-		style: ButtonStyle.Primary,
-		type: ComponentType.Button,
-		label: `Option ${entryIndex}`,
-		customId: "@sapphire/paginated-messages.stop",
-		run(context: PaginatedMessageActionContext) {
-			const modal = new ModalBuilder()
-				.setCustomId(
-					new CustomId({
-						type: "reminder",
-						reminderId: reminder.id.toString(),
-					}).toString()
-				)
-				.setTitle("Edit Reminder")
-				.setComponents([row]);
-			context.interaction.showModal(modal);
-		},
-	};
 }
--- src/models/pagination/reminderComponentData.ts
+++ src/models/pagination/reminderComponentData.ts
@@ -1,5 +1,11 @@
-import { PaginatedMessageAction } from "@sapphire/discord.js-utilities";
-import { Collection, EmbedField } from "discord.js";
+import { reminders } from "@prisma/client";
+import {
+	PaginatedMessageAction,
+	PaginatedMessageActionContext,
+} from "@sapphire/discord.js-utilities";
+import { ButtonStyle, Collection, ComponentType, EmbedField, ModalBuilder } from "discord.js";
+import { CustomId } from "./CustomId";
+import { createReminderRowComponent } from "./paginationRowData";
 
 /**
  * This class is a flattened data structure with all the data required to produce one embed entry and one associated page action.
@@ -95,4 +101,50 @@ export class ReminderPage extends Collection<bigint, ReminderComponentData> {
 			return component.button;
 		});
 	}
+
+	public static fromReminders(reminders: reminders[]) {
+		const collection = new ReminderPage();
+		reminders.forEach((reminder, entryIndex) => {
+			collection.set(reminder.id, convertReminder(reminder, entryIndex));
+		});
+		return collection;
+	}
+}
+
+/**
+ * Converts a reminder object to a ReminderPaginationData object.
+ * @param reminder - The reminder object to convert.
+ * @param entryIndex - The index of the entry.
+ * @returns The converted ReminderPaginationData object.
+ */
+function convertReminder(reminder: reminders, entryIndex: number): ReminderComponentData {
+	const data: ReminderComponentData = new ReminderComponentData({
+		id: reminder.id,
+		message: reminder.reminder_message,
+		time: reminder.time,
+		button: createButton(entryIndex, reminder),
+	});
+	return data;
+}
+
+function createButton(entryIndex: number, reminder: reminders): PaginatedMessageAction {
+	const row = createReminderRowComponent(reminder.reminder_message, "");
+	return {
+		style: ButtonStyle.Primary,
+		type: ComponentType.Button,
+		label: `Option ${entryIndex}`,
+		customId: "@sapphire/paginated-messages.stop",
+		run(context: PaginatedMessageActionContext) {
+			const modal = new ModalBuilder()
+				.setCustomId(
+					new CustomId({
+						type: "reminder",
+						reminderId: reminder.id.toString(),
+					}).toString()
+				)
+				.setTitle("Edit Reminder")
+				.setComponents([row]);
+			context.interaction.showModal(modal);
+		},
+	};
 }
