84cb3e1709204b3cfb1f04b58430423e6251fdc3
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-30 15:28:03 -0700

    convenience function to get string directly on stack

--- src/utils/stackUtils.ts
+++ src/utils/stackUtils.ts
@@ -8,11 +8,18 @@ export function stripDirectoryPath(dir: string) {
 	return dir.slice(res)
 }
 
-export function parseToCleanedStackframe(stackTrace: string): StackFrame[] {
+export function parseToCleanedStackframe(stackTrace: string | undefined): StackFrame[] {
+	stackTrace = stackTrace ?? ""
 	return parse(stackTrace).map((frame) => {
 		if (!frame.file) {
 			throw new Error("Unexpectedly did not find file path in stack frame")
 		}
 		return { ...frame, file: stripDirectoryPath(frame.file) }
 	})
+}
+
+export function parseToCleanedStackFrameString(stackTrace: string | undefined): string {
+	return parseToCleanedStackframe(stackTrace)
+		.map((frame) => frame.file)
+		.join("\n")
 }
